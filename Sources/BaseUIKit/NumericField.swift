f72a8712e82bc489ee6414e7a1f159ccab406769
Andrew Finnell <andy@losingfight.com>
2024-06-29 11:28:37 -0400

    Basic numeric field
    Works better on macOS than iOS. Formatter allows too much on iOS

---
+++ Sources/BaseUIKit/NumericField.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+public struct NumericField: View {
+    private static let numberFormater: NumberFormatter = {
+       let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    let title: String
+    let value: Binding<Double>
+    let errorMessage: String?
+    
+    public init(
+        _ title: String,
+        value: Binding<Double>,
+        errorMessage: String? = nil
+    ) {
+        self.title = title
+        self.value = value
+        self.errorMessage = errorMessage
+    }
+    
+    public var body: some View {
+        VStack {
+            TextField(
+                title,
+                value: value,
+                formatter: NumericField.numberFormater,
+                prompt: Text(title)
+            )
+            #if os(iOS)
+            .keyboardType(.decimalPad)
+            #endif
+            .autocorrectionDisabled(true)
+            .multilineTextAlignment(.trailing)
+            .frame(idealWidth: 120, maxWidth: 120)
+            
+            if let errorMessage {
+                Text(errorMessage)
+                    .font(.footnote)
+                    .foregroundStyle(Color.red)
+            }
+        }
+    }
+}
